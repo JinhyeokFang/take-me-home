7e5fec5d3365a3cde02cc864e78a464aea9d7061
jinhyeokfang <jinhyeokfang@gmail.com>
2023-01-13 14:32:46 +0000

    chore: remove compiled code

--- build/owner/domain/owner-type.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.OwnerType = void 0;
-var OwnerType;
-(function (OwnerType) {
-    OwnerType[OwnerType["INDIVIDUAL"] = 0] = "INDIVIDUAL";
-    OwnerType[OwnerType["SHELTER"] = 1] = "SHELTER";
-})(OwnerType = exports.OwnerType || (exports.OwnerType = {}));
-//# sourceMappingURL=owner-type.js.map
--- build/owner/domain/pet/information/species.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Species = void 0;
-var Species;
-(function (Species) {
-    Species[Species["DOG"] = 0] = "DOG";
-    Species[Species["CAT"] = 1] = "CAT";
-})(Species = exports.Species || (exports.Species = {}));
-//# sourceMappingURL=species.js.map
