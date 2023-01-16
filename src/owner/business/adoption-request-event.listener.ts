c47695abd29497624627f7f378dcf78e27b7e415
jinhyeokfang <jinhyeokfang@gmail.com>
2023-01-16 14:37:46 +0000

    refactor(adoption-request-event-listener): getPetFromShelter()

--- src/owner/business/adoption-request-event.listener.ts
+++ src/owner/business/adoption-request-event.listener.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { OnEvent } from '@nestjs/event-emitter';
+import { Owner } from '../domain/owner';
+import { PetID } from '../domain/pet/pet-id';
 import { OwnerMysqlRepository } from '../infrastructure/owner.mysql.repository';
 import { AdoptionRequestAcceptedEvent } from './adoption-request-accepted.event';
 
@@ -12,14 +14,18 @@ export class AdoptionRequestEventListener {
   ) {
     const requestor = await this.ownerRepository.findOneById(event.requestorId);
     const shelter = await this.ownerRepository.findOneById(event.shelterId);
+    const pet = this.getPetFromShelter(shelter, event.petId);
 
-    if (!shelter.hasPet(event.petId)) {
-      throw new Error('Shelter does not have the pet');
-    }
-
-    const pet = shelter.getPetLists().find((pet) => pet.id === event.petId);
     requestor.adoptPet(pet, shelter);
 
     await this.ownerRepository.save(requestor);
+  }
+
+  private getPetFromShelter(shelter: Owner, petId: PetID) {
+    if (!shelter.hasPet(petId)) {
+      throw new Error('Shelter does not have the pet');
+    }
+
+    return shelter.getPetLists().find((pet) => pet.id === petId);
   }
 }
